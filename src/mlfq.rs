61c8b9015a7a62db22f2c5a9424be0d920007a88
GCYY <734536637@qq.com>
2019-03-13 13:21:40 +0800

    OSTEP

--- src/mlfq.rs
+++ src/mlfq.rs
@@ -1,7 +1,7 @@
 // not done
 
 
-struct mlfq_option {
+struct MlfqOption {
     seed:i32,
     numQueues:i32,
     quantum:i32,
@@ -17,9 +17,9 @@ struct mlfq_option {
     solve:bool,
 }
 
-impl mlfq_option {
-    fn new() -> mlfq_option {
-        mlfq_option {
+impl MlfqOption {
+    fn new() -> MlfqOption {
+        MlfqOption {
             seed:0,
             numQueues:3,
             quantum:10,
@@ -42,5 +42,40 @@ fn FindQueue() {
 }
 
 pub fn mlfq_op_parse(op_vec:Vec<&str>) {
+    let mut mlfq_op = MlfqOption::new();
+    let mut i =1;
+    while i<op_vec.len() {
+        match op_vec[i] {
+            "-s" =>{mlfq_op.seed = op_vec[i+1].parse().unwrap();i= i+2;},
+            "-n" =>{mlfq_op.numQueues = op_vec[i+1].parse().unwrap();i = i+2;},
+            "-q" =>{mlfq_op.quantum = op_vec[i+1].parse().unwrap();i=i+2;},
+            "-Q" =>{mlfq_op.quantumList = op_vec[i+1].to_string();i=i+2;},
+            "-j" =>{mlfq_op.numJobs = op_vec[i+1].parse().unwrap();i=i+2;},
+            "-m" =>{mlfq_op.maxlen = op_vec[i+1].parse().unwrap();i= i+2;},
+            "-M" =>{mlfq_op.maxio =op_vec[i+1].parse().unwrap();i= i+2;},
+            "-B" =>{mlfq_op.boost = op_vec[i+1].parse().unwrap();i= i+2;},
+            "-i" =>{mlfq_op.ioTime = op_vec[i+1].parse().unwrap();i = i+2;},
+            "-S" =>{mlfq_op.stay = true;i=i+1;},
+            "-I" =>{mlfq_op.iobump = true;i=i+1;},
+            "-l" =>{mlfq_op.jlist = op_vec[i+1].to_string();i=i+2;},
+            "-c" =>{mlfq_op.solve = true;i=i+1;},
+            _ => println!("mlfq_op_parse match wrong!!"),
+        }
+    }
+    execute_mlfq_op(mlfq_op);
+}
 
+fn execute_mlfq_op(options:MlfqOption) {
+
+    let seed_u8 = options.seed as u8;
+    let seed = [seed_u8+1,seed_u8+2,seed_u8+3,seed_u8+4,
+                                    seed_u8+5,seed_u8+6,seed_u8+7,seed_u8+8,
+                                    seed_u8+9,seed_u8+10,seed_u8+11,seed_u8+12,
+                                    seed_u8+13,seed_u8+14,seed_u8+15,seed_u8+16];
+
+    let mut rng = SmallRng::from_seed(seed);
+
+    let numQueues = options.numQueues;
+
+    let quantum = HashMap::new();
 }
